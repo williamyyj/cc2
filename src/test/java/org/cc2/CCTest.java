03b3f753dfdcf192a602e8d4e1ddc45c1aa60a3e
williamyyj <williamyyj@gmail.com>
2014-08-26 11:01:26 +0800

    base-line

--- src/test/java/org/cc2/CCTest.java
+++ src/test/java/org/cc2/CCTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.cc2;
 
+import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,32 +18,38 @@ import static org.junit.Assert.*;
  * @author William
  */
 public class CCTest {
-    
+
     public CCTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-
     @Test
     public void hello() {
-        String prjId = "prj/baphiq" ; 
+        String prjId = "prj/baphiq";
         String base = CCConst.base(prjId);
-        CCMap m = CCCache.load_map(base, "cfg","json");
+        CCMap m = CCCache.load_map(base, "cfg", "json");
         System.out.println(m.toString("\t"));
     }
+
+    @Test
+    public void print_list() {
+        CCList cl = new CCList(new Object[]{1, true, 3, "4", 5, 6, 7, 8, 9, 10, new Date()});
+        System.out.println(cl.toString("  "));
+    }
+    
 }
